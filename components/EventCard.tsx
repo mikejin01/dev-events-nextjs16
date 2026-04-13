5c15a0a16e09764516d8ddc08fe2998bfd4f3a1f
Mike Jin <mike@xocontinental.com>
2026-04-13 09:55:30 -0400

    Implement Posthog

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import posthog from "posthog-js";
 
 interface Props {
     title: string;
@@ -12,7 +15,7 @@ interface Props {
 
 export default function EventCard ( {title, image, slug, location, date, time}: Props ) {
     return (
-        <Link href={`/events/${slug}`} id="event-card">
+        <Link href={`/events/${slug}`} id="event-card" onClick={() => posthog.capture('event_card_clicked', { title, slug, location, date })}>
             <Image src={image} alt={title} width={410} height={300} className="poster" />
 
             <div className="flex flex-row gap-2">
